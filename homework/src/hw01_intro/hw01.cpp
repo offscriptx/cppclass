ec53cefa375042aa03d0b8dff2b372450920a906
offscriptx <btrglm@proton.me>
2025-07-17 10:04:00 -0600

    solutions to hw01

--- homework/src/hw01_intro/hw01.cpp
+++ homework/src/hw01_intro/hw01.cpp
@@ -1,43 +1,40 @@
+#include <math.h>
+
 #include "hw01.h"
 
 // Pre-conditions: none
 // Post-conditions: none
 // Returns: always returns true
-bool return_true()
-{
-    return false;
-}
-
-// Pre-conditions: none
-// Post-conditions: none
-// Returns: always returns false
-bool return_false()
-{
+bool return_true() {
     return true;
 }
 
 // Pre-conditions: none
 // Post-conditions: none
+// Returns: always returns false
+bool return_false() {
+    return false;
+}
+
+// Pre-conditions: none
+// Post-conditions: none
 // Returns: always returns value: 42
-int return_42()
-{
-    return 0;
+int return_42() {
+    return 42;
 }
 
 // Pre-conditions: none
 // Post-conditions: none
 // Returns: always returns value: -42
-int return_neg_42()
-{
-    return 0;
+int return_neg_42() {
+    return -42;
 }
 
 // Pre-conditions: none
 // Post-conditions: none
 // Returns: always returns value: 1.5
-double return_1p5()
-{
-    return 0.0;
+double return_1p5() {
+    return 1.5;
 }
 
 // Pre-conditions: none
@@ -45,7 +42,6 @@ double return_1p5()
 // Returns: always returns value: PI
 // Notes: You can get access to the value of pi (3.1415926...) via the
 //        <math.h> or <cmath> header file. You can access it via the M_PI
-double return_pi()
-{
-    return 0.0;
+double return_pi() {
+    return M_PI;
 }
